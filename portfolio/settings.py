a8fdfc10674792d12d456d8d1090497028d15a4b
Tumhara-GitHub-Username <dadmuhammad2005@gmail.com>
2026-05-23 01:13:42 +0500

    Render deployment setup

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -149,6 +149,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
